9a3b775ce5c54dc4d5a2f371fd797fda4e5842e2
Marco Tulio Masselli Rainho Teixeira <56456537+marcotuliomrt@users.noreply.github.com>
2022-05-08 21:42:24 -0300

    Create utilities.h

---
+++ utilities.h
@@ -0,0 +1,17 @@
+/***********************************************************************
+* 09-05-2022
+* 2x Motor-de-passo + Joystick (X-Y)
+************************************************************************/
+
+
+//------------------- funcoes pqra mover motor do eixo x ------------------------
+void MoveX(int x);
+
+// funcao pqra mover motor do eixo y
+void MoveY(int y);
+
+//---------------------------- rotina de jogging -------------------------------
+void Jogging();
+
+//--------------------------- funcao que guarda posicoes --------------------------
+void GetPosition();
